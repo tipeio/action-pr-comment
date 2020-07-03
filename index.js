27359982a76b75b6a35762849aed75ed46ac2681
Scott Moss <willscottmoss@gmail.com>
2020-07-02 20:17:12 -0700

    init

---
+++ index.js
@@ -0,0 +1,24 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+(async () => {
+  try {
+    const userMap = JSON.parse(core.getInput('userMap'))
+    const commentPayload = github.context.payload.pull_request_review_comment
+    const slackToken = core.getInput('slackToken')
+
+    const app = new App({
+      token: slackToken,
+      signingSecret: core.getInput('slackSigningSecret')
+    })
+
+    const result = await app.client.chat.postMessage({
+      token: slackToken,
+      channel: 'D781H60V9',
+      text: commentPayload.body
+    });
+    
+  } catch (e) {
+    core.setFailed(error.message)
+  }
+})()
